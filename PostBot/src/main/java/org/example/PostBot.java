239e096da23210f5823ac584225e45a6c25830bb
Dan <danil.motor9876@gmail.com>
2023-09-28 14:15:00 +0500

    echo

--- PostBot/src/main/java/org/example/PostBot.java
+++ PostBot/src/main/java/org/example/PostBot.java
@@ -1,13 +1,21 @@
 package org.example;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class PostBot extends TelegramLongPollingBot
 {
     @Override
     public void onUpdateReceived(Update update)
     {
-        System.out.println(update);
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText());
+        System.out.println(user.getFirstName() + " wrote " + msg.getText());
+
     }
 
     @Override
@@ -20,5 +28,18 @@ public class PostBot extends TelegramLongPollingBot
     public String getBotToken()
     {
         return "6474374849:AAEOOTTDQaLDvgs6iXt8WYhldgYeDm5A6Xg";
+    }
+
+    public void sendText(Long who, String what)
+    {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString())
+                .text(what).build();
+        try
+        {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
