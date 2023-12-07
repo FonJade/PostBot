179e3935b733363d8c31a588b3a481ba368b7dbf
FonJade <shterbov.zakhar@yandex.ru>
2023-12-07 15:52:16 +0500

    Added DataBase, work only with vk

---
+++ PostBot/src/main/java/org/example/Connect.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "\\..\\DataBase.db";
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+}
